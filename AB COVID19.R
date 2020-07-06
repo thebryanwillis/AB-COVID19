4f483acb91c47d5d6c207d4db662d43b71be3c03
Bryan Willis <66087320+thebryanwillis@users.noreply.github.com>
2020-07-06 16:52:20 -0400

    R Script

--- AB COVID19.R
+++ AB COVID19.R
@@ -24,3 +24,13 @@ p2 = p2 + facet_wrap(~Alberta.Health.Services.Zone) +transition_time(Day.Count)+
 animate(p2, duration = 7, fps = 25, width = 800, height = 450, renderer = gifski_renderer())
 anim_save("death.gif")
 
+#Filter data to only show day 120
+covid120 = covid %>% filter(Day.Count == 120)
+#Produce graph to show total cases
+case = ggplot(covid120, aes( y = Case.Count, x=Alberta.Health.Services.Zone, colour = Alberta.Health.Services.Zone))+geom_boxplot(show.legend = FALSE)+labs(x="Alberta Health Zone", y = "# of Cases")+theme(text = element_text(size=10), axis.text.x = element_text(angle=30, hjust=1))+geom_text(aes(label = Case.Count), size = 3, position=position_dodge(width=0.9), vjust=-0.5, show.legend = FALSE)
+case
+ggsave("case.png")
+#Produce graph to show total deaths
+death = ggplot(covid120, aes( y = Death.Count, x=Alberta.Health.Services.Zone, colour = Alberta.Health.Services.Zone))+geom_boxplot(show.legend = FALSE)+labs(x="Alberta Health Zone", y = "# of Deaths")+theme(text = element_text(size=10), axis.text.x = element_text(angle=30, hjust=1))+geom_text(aes(label = Death.Count), size = 3, position=position_dodge(width=0.9), vjust=-0.5, show.legend = FALSE)
+death
+ggsave("death.png")
